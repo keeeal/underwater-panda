d321a332b0171023c2579323f12a42332a8a1d10
Keeeal <james@keal.com.au>
2020-08-25 11:55:38 +0930

    working controls

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 
+import os, sys, json
+
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFile
 
 config_dir = 'config'
-loadPrcFile(os.path.join(config_dir, 'config.prc'))
+config = os.path.join(config_dir, 'config.prc')
+controls = os.path.join(config_dir, 'controls.json')
+loadPrcFile(config)
 
 class Game(ShowBase):
     def __init__(self):
         super().__init__()
 
-        self.keys_pressed = {}
-        controls = os.path.join(config_dir, 'controls.json')
         self.load_controls(controls)
 
         self.taskMgr.add(self.loop, 'loop')
 
-    def set_key(self, key, value):
-        self.keys_pressed[key] = value
-
-    def load_controls(self, controls):
+    def load_controls(self, controls: str):
         with open(controls) as f:
             controls = json.load(f)
 
+        self.actions = {a: False for a in controls.values()}
+
+        def set_action(action: str, value: bool):
+            self.actions[action] = value
+
         for key, action in controls.items():
-            self.accept(key, self.set_key, [key, True])
-            self.accept(key + '-up', self.set_key, [key, False])
+            self.accept(key, set_action, [action, True])
+            self.accept(key + '-up', set_action, [action, False])
 
     def loop(self, task):
+        for action, value in self.actions.items():
+            if value:
+                print(action)
+
+        if self.actions['exit']:
+            sys.exit()
 
         return task.cont
 
